395e26e9caaf55a149d9d6e5d5f715ba72aa4bfd
TheNanoBay <noorani.mehdi@outlook.com>
2021-01-16 20:35:26 -0600

    commit tree c9bde8df6746a4062983c390cc9520c3058333f8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,14 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id]) #finds the specific student via id 
+    @student.active = !@student.active  #toggles to the oppostie of what the student currnetly is for active 
+    @student.save #saves the new changed active status in the DB so it persists 
+    redirect_to student_path(@student) #redirects to the speficic student page 
+  end
+
+
   private
 
     def set_student
